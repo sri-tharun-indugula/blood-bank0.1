527c2fad68601e6e44b104061574d182ac1730a8
sri-tarun-indugula <indugulasritharun123@gmail.com>
2024-02-21 14:28:16 +0530

    added name change to spect file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Blood-bankk-app0.1'`, () => {
+  it(`should have as title 'Blood-bankk-app0'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Blood-bankk-app0.1');
+    expect(app.title).toEqual('Blood-bankk-app0');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Blood-bankk-app0.1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Blood-bankk-app0 app is running!');
   });
 });
